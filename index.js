b40347c8b7fcf15ce439d2cda346368f353eb99d
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-29 03:29:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 let message;
 
 function scuberGreetingForFeet(num) {
-  message = "This one is on me!"
   if (num <= 400) {
-    return message
+    return "This one is on me"
   } else if (num > 2000) {
     message = "I will gladly take your thirty bucks."
   } else if (num > 2500) {
